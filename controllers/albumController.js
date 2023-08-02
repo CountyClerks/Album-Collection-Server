df71b69c2b9071b3a7e250fb041d64a20e7127f1
Eric Hardy <erichardy011@gmail.com>
2023-08-01 22:49:48 -0500

    Boilerplate for GET/POST CRUD operations for artist controller

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -10,8 +10,6 @@ const { body, validationResult } = require('express-validator')
 //REASON: first time building a backend with a separated front end
 
 exports.index = asyncHandler(async (req, res, next) => {
-    //Get details of artists, albums, and genres
-
     const [
         numArtists,
         numAlbums,
@@ -75,6 +73,8 @@ exports.album_create_post = [
         }
         next()
     },
+
+    //Validate and sanitize fields.
     body("title", "Title must not be empty.")
         .trim()
         .isLength({ min: 1 })
@@ -142,6 +142,149 @@ exports.album_create_post = [
         } else {
             await album.save()
             res.redirect(album.url)
+        }
+    })
+]
+
+exports.album_delete_get = asyncHandler(async (req, res, next) => {
+    const album = await Promise(Album.findById(req.paramrs.id).populate("artist").populate("genre").exec())
+    if (album === null) {
+        res.redirect("/catalog/albums")
+    }
+
+    res.render("album_delete", {
+        title: "Delete Album",
+        album: album,
+    })
+})
+
+exports.album_delete_post = asyncHandler(async (req, res, next) => {
+    const album = await Promise(Album.findById(req.paramrs.id).populate("artist").populate("genre").exec())
+
+    if (album === null) {
+        res.redirect("/catalog/album")
+    }
+    await Album.findByIdAndRemove(req.body.id)
+    res.redirect("/catalog/albums")
+})
+
+exports.album_update_get = asyncHandler(async (req, res, next) => {
+    const [album, allArtists, allGenres] = await Promise.all([
+        Album.findById(req.params.id).populate("artist").populate("genre").exec(),
+        Artist.find().exec(),
+        Genre.find().exec(),
+    ])
+
+    if (book === null) {
+        const err = new Error("Album not found")
+        err.status = 404
+        return next(err)
+    }
+
+    for (const genre of allGenres) {
+        for (const album_g of album.genre) {
+            if (genre._id.toString() === album_g._id.toString())  {
+                genre.checked = "genre"
+            }
+        }
+    }
+
+    res.render("album_form", {
+        title: "Update Album",
+        artists: allArtists,
+        genres: allGenres,
+        album: album,
+    })
+})
+
+exports.album_update_post = [
+    (req, res, next) => {
+        if (!(req.body.genre instanceof Array)) {
+            if (typeof req.body.genre === undefined) {
+                req.body.genre = []
+            } else {
+                req.body.genre = new Array(req.body.genre)
+            }
+        }
+        next()
+    },
+
+    //Validate and sanitize fields.
+    body("title", "Title must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("artist", "Author must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("description", "Description must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("price", "Price must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("quantity", "Quantity must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("release_date", "Release date must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("covert_art", "Covert art must not be empty.")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("genre.*").escape(),
+    
+    asyncHandler(async (req, res, next) => {
+        //Get validation errors from request
+        const errors = validationResult(req)
+        
+        //Create Album object with trimmed data and old id
+        const album = new Album({
+            title: req.body.title,
+            artist: req.body.artist,
+            description: req.body.description,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            release_date: req.body.release_date,
+            genre: typeof req.body.genre === "undefined" ? [] : req.body.genre,
+            cover_art: req.body.cover_art,
+            _id: req.params.id,
+        })
+        
+        if(!errors.isEmpty()) {
+            //No errors. Render form again with sanitized values.
+
+            //Get all artists and genres for the form.
+            const [allArtists, allGenres] = await Promise.all([
+                Artist.find().exec(),
+                Genre.find().exec(),
+            ])
+            //Mark genre as selected.
+            for (const genre of allGenres) {
+                if (album.genre.indexOf(genre._id) > -1) {
+                    genre.checked = "true"
+                }
+            }
+            res.render("album_form", {
+                title: "Update Book",
+                artists: allArtists,
+                genres: allGenres,
+                album: album,
+                errors: errors.array()
+            })
+            return;
+        } else {
+            //If data form is valid, update record.
+            const thealbum = await Book.findByIdAndUpdate(req.params.id, album, {})
+
+            //Redirect to album details page
+            res.redirect(thealbum.url)
         }
     })
 ]
--- controllers/artistController.js
+++ controllers/artistController.js
@@ -4,3 +4,60 @@ const Album = require('../models/album')
 const asyncHandler = require('express-async-handler')
 const { body, validationResult } = require('express-validator')
 
+
+//Display list of all Artists.
+exports.artist_list = asyncHandler(async (req, res, next) => {
+
+    const allArtists = await Artist.find().sort({last_name: 1}).exec()
+
+    res.render("artist_list", {
+        title: "Artist List",
+        artist_list: allArtists,
+    })
+})
+
+//Display detail page for a specific Artist
+exports.artist_detail = asyncHandler(async (req, res, next) => {
+
+    const [artist, allAlbumsByArtist] = await Promise.all([
+        Artist.findById(req.params.id).exec(),
+        Album.find({ artist: req.params.id }, "title description").exec(),
+    ])
+    //Throw error if no artist exist
+    if (artist === null) {
+        const err = new Error("Artist not found")
+        err.status = 404
+        return next(err)
+    }
+
+    res.render("artist_detail", {
+        title: "Artist Detail",
+        artist: artist,
+        artist_albums: allAlbumsByArtist
+    })
+})
+
+//GET form for creating artist
+exports.artist_create_get = (req, res, next) => {
+    res.render("artist_form", { title: "Create Artist"})
+}
+
+exports.artist_create_post = [
+
+]
+
+exports.artist_delete_get = asyncHandler(async (req, res, next) => {
+
+})
+
+exports.artist_delete_post = asyncHandler(async (req, res, next) => {
+
+})
+
+exports.artist_update_get = asyncHandler(async (req, res, next) => {
+
+})
+
+exports.artist_update_post = asyncHandler(async (req, res, next) => {
+
+})
